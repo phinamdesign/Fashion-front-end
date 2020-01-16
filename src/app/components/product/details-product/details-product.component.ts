a7950b827d389a45ec9029e5a6f750f0345b7bfa
Nghiemducnam <Nghiemducnam0312@gmail.com>
2020-01-16 10:50:46 +0700

    move components dictionary

---
+++ src/app/components/product/details-product/details-product.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute} from '@angular/router';
+import { Product } from '../../../models/product';
+import { ProductService } from '../../../services/product.service';
+
+@Component({
+  selector: 'app-details-product',
+  templateUrl: './details-product.component.html',
+  styleUrls: ['./details-product.component.css']
+})
+export class DetailsProductComponent implements OnInit {
+  id: number;
+  product: Product;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.product = new Product();
+    this.id = this.route.snapshot.params.id;
+    this.productService.getProduct(this.id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.product = data;
+        }, error => console.log(error));
+  }
+
+  listProduct() {
+    this.router.navigate(['product']);
+  }
+
+}
