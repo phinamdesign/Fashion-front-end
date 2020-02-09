5dcd6f04f6e862b937b8b05f278f7d25e8e8c956
Phi Nam <phinamhd1992@gmail.com>
2020-02-09 15:29:49 +0700

    setup content header and update slide show

--- src/app/components/Structure/header/header.component.ts
+++ src/app/components/Structure/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {SearchProductByName} from '../../../models/SearchProductByName';
+import {ProductService} from '../../../services/product.service';
+import {Product} from '../../../models/product';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  private nameProduct = '';
+  content: string;
+  listProduct: Product[] = [];
+  productForm = new FormGroup({
+    name: new FormControl('')
+  });
 
-  constructor() { }
+  constructor(
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
+    this.productService.getListProduct().subscribe(next =>
+      (this.listProduct = next), err =>
+      (this.content = this.content = JSON.parse(err.error).message));
+  }
+
+  searchProductByName() {
+    const nameForm: SearchProductByName = {
+      nameProduct: this.nameProduct
+    };
+    this.productService.searchProductByName(nameForm).subscribe(
+      result => {
+        this.listProduct = result;
+      }, error => {
+        console.log(error);
+      }
+    );
   }
 
 }
